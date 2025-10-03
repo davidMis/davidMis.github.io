fe3ef81885c569f0b93ba2c24d4abd512b023974
David <d.mis09@gmail.com>
2025-10-03 16:38:44 -0500

    Added sitemap

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@ quarto render index.qmd --to pdf
 mv docs/index.pdf files/DavidMis-CV.pdf
 quarto render
 cp files/googlee496e0ae7810f83e.html docs/googlee496e0ae7810f83e.html
+cp files/robots.txt docs/robots.txt
+cp files/sitemap.xml docs/sitemap.xml
